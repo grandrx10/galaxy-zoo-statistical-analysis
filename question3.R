53a3ca285d733a566493b987ab537b45644973bc
sealine-e <125224850+sealine-e@users.noreply.github.com>
2023-03-08 16:33:18 -0500

    Update question3.R

--- question3.R
+++ question3.R
@@ -4,7 +4,17 @@ library(arrow)
 df2 <- read_parquet("nsa_v1_0_1_key_cols.parquet")
 glimpse(df2)
 
+# scatterplot
 ggplot(data = df2) + 
   aes(x = redshift, y = petro_theta) + 
   geom_point() + 
   labs(title = "Redshift Compared to the Radius of the Galaxy", x = 'Redshift', y = 'Radius of Galaxy')
+
+cor(df2$redshift, df2$petro_theta)
+
+# scatterplot with line of best fit
+ggplot(data = df2) + 
+  aes(x = redshift, y = petro_theta) + 
+  geom_point() + geom_smooth(method=lm, se=FALSE) + 
+  labs(title = "Redshift Compared to the Radius of the Galaxy", x = 'Redshift', y = 'Radius of Galaxy')
+
