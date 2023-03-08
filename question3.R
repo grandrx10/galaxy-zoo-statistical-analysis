5996526df684cef38839df18628f8a40436d3080
sealine-e <125224850+sealine-e@users.noreply.github.com>
2023-03-08 16:26:05 -0500

    Update question3.R

--- question3.R
+++ question3.R
@@ -3,3 +3,8 @@ library(arrow)
 
 df2 <- read_parquet("nsa_v1_0_1_key_cols.parquet")
 glimpse(df2)
+
+ggplot(data = df2) + 
+  aes(x = redshift, y = petro_theta) + 
+  geom_point() + 
+  labs(title = "Redshift Compared to the Radius of the Galaxy", x = 'Redshift', y = 'Radius of Galaxy')
