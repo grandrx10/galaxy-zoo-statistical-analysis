ca3c75fa732695165f705be9873c516df8fa06eb
finesden33 <113128063+finesden33@users.noreply.github.com>
2023-03-08 15:46:38 -0500

    Add files via upload

--- question1.R
+++ question1.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(arrow)
 
-df <- read_parquet("gz2_catalog_with_modern_schema_no_paths.parquet")
+df2 <- read_parquet("gz2_catalog_with_modern_schema_no_paths.parquet")
 glimpse(df)
 
+# This is a change
