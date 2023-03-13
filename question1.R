e3b2fb34d3de106e57a94f3a99f30a192161cc04
Ethan Fine <finesden33#gmail.com>
2023-03-13 16:52:38 -0400

    Visualizations and Hypothesis test

--- question1.R
+++ question1.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(arrow)
+set.seed(971)
 
 df <- read_parquet("gz2_catalog_with_modern_schema_no_paths.parquet")
 
@@ -28,10 +29,56 @@ df <- mutate(df, most_can_tell = voter_confidence >= 0.5)
 glimpse(df)
 head(df)
 
+library(ggplot2)
+
+# visualization 1 (bar plot)
+dfsum1 <- df %>% group_by(most_can_tell, more_than_four_consensus) %>% tally()
+
+dfsum1 %>% ggplot(aes(x = most_can_tell, y = n, fill = more_than_four_consensus, colour = more_than_four_consensus)) + 
+  geom_bar(stat = "identity", position = 'dodge', alpha = 0.5) +
+  labs(x="Most people were confident voters", y="Number of Galaxies", fill="Most people voted more than 4") +
+  guides(more_than_four_consensus = FALSE) + theme_bw()
+
+# visualization 2 (histogram)
+df %>% ggplot(aes(x = voter_confidence, fill=more_than_four_consensus)) + geom_histogram(position = "identity", alpha = 0.5, bins=10) +
+  labs(x="Voter confidence", y="Number of Galaxies", fill='Most people voted more than 4') + 
+  ylim(0, 31000) + theme_bw()
+
+
+# hypothesis test
 param_stat <- sum(df$more_than_four_consensus)/length(df$iauname)
 
 can_stat <- sum(df$more_than_four_consensus & df$most_can_tell)/sum(df$most_can_tell)
 
 cant_stat <- sum(df$more_than_four_consensus & !df$most_can_tell)/sum(!df$most_can_tell)
 
-#we want to do a hypothesis test, null hyp is that can_stat = cant_stat, alt hyp is that can != cant_stat
+# null hyp is that can_stat = cant_stat, 
+# alt hyp is that can_stat != cant_stat
+
+test_stat <- cant_stat
+repetitions <- 5000
+simulated_values <- rep(NA, repetitions)
+
+for(i in 1:repetitions){
+  # perform a random permutation and compute the simulated test statistic
+  simdata <- df %>%
+    mutate(more_than_four_consensus = sample(more_than_four_consensus, replace=FALSE))
+  
+  # re-compute the test statistic
+  sim_value <- sum(simdata$more_than_four_consensus & !simdata$most_can_tell)/sum(!simdata$most_can_tell)
+  
+  # store the simulated value
+  simulated_values[i] <- as.numeric(sim_value)
+}
+
+# convert vector results to a tibble
+sim <- tibble(median_diff = simulated_values)
+
+# plot the results
+sim %>% ggplot(aes(x=median_diff)) + geom_histogram()
+
+# Check p-value
+alpha <- 0.05
+p_val = 2 * sum(simulated_values >= test_stat) / repetitions
+print(paste(c('p value:', p_val)))
+print(ifelse(p_val < alpha, 'Reject the null', 'Fail to reject the null'))
